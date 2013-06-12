40ad7e04768ada081d30aa42389f79ee6a404f2b
Gustavo <gchalcoa@gmail.com>
2013-06-11 20:59:18 -0500

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterRatingUPC::Application.config.secret_token = '5f7a4744521a57537f109cd985fd3e5ad69fdcbb67562fbc5009ebe8aae95c3e3ecc1ccdad8ecee8fa61ea795016656d0e1f88177504777d7967e3cbc5f37478'
